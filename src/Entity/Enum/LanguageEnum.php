5c505210f8d757db49d01a512c2890375c8de430
Daniel Borges <d.rockemback@gmail.com>
2025-09-01 22:07:01 -0300

    Adicinando as flags e valores corretos para cada descrição

---
+++ src/Entity/Enum/LanguageEnum.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Entity\Enum;
+
+enum LanguageEnum: int
+{
+    case PORTUGUESE = 1;
+    case SPANISH = 2;
+    case ENGLISH = 3;
+
+    public static function getOptions(): array
+    {
+        return [
+            'Português' =>  self::PORTUGUESE->value,
+            'Espanhol' => self::SPANISH->value,
+            'Inglês' => self::ENGLISH->value,
+        ];
+    }
+
+    public static function getDescription(?int $idLanguage): string
+    {
+        $languagesDescription = [
+            self::PORTUGUESE->value => 'Português',
+            self::SPANISH->value =>  'Espanhol',
+            self::ENGLISH->value =>  'Inglês',
+            '' => ''
+        ];
+
+        return $languagesDescription[$idLanguage];
+    }
+
+    public static function getFlag(?int $idLanguage)
+    {
+        $baseUrl = 'public/assets/flags/';
+
+        $languagesDescription = [
+            self::PORTUGUESE->value => '<div class="d-none">pt</div><img src="/assets/images/flags/br.svg" width="20"> Português',
+            self::SPANISH->value =>  '<div class="d-none">es</div><img src="/assets/images/flags/es.svg" width="20"> Espanhol',
+            self::ENGLISH->value =>  '<div class="d-none">uk</div><img src="/assets/images/flags/uk.svg" width="20"> Inglês',
+            '' => ''
+        ];
+
+        return $languagesDescription[$idLanguage];
+    }
+
+}
