9602ef180f810e6cd5e284d366622656fcff3c87
Daniel Borges <d.rockemback@gmail.com>
2025-09-21 10:33:58 -0300

    Criando Crud, Controller e Entity de Depoimentos

---
+++ src/Entity/Testimony.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ServiceRepository;
+use App\Repository\TestimonyRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+#[ORM\Entity(repositoryClass: TestimonyRepository::class)]
+#[Vich\Uploadable]
+class Testimony
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $shortDescripton = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $fullDescription = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $status = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $highlighted = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $position = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $image = null;
+    #[Vich\UploadableField(mapping: 'testimonyImage', fileNameProperty: 'image')]
+    private ?File $imageFile = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $imageUpdatedAt = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $language = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(?string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getShortDescripton(): ?string
+    {
+        return $this->shortDescripton;
+    }
+
+    public function setShortDescripton(?string $shortDescripton): static
+    {
+        $this->shortDescripton = $shortDescripton;
+
+        return $this;
+    }
+
+    public function getFullDescription(): ?string
+    {
+        return $this->fullDescription;
+    }
+
+    public function setFullDescription(?string $fullDescription): static
+    {
+        $this->fullDescription = $fullDescription;
+
+        return $this;
+    }
+
+    public function isStatus(): ?bool
+    {
+        return $this->status;
+    }
+
+    public function setStatus(?bool $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function isHighlighted(): ?bool
+    {
+        return $this->highlighted;
+    }
+
+    public function setHighlighted(?bool $highlighted): static
+    {
+        $this->highlighted = $highlighted;
+
+        return $this;
+    }
+
+    public function getPosition(): ?int
+    {
+        return $this->position;
+    }
+
+    public function setPosition(?int $position): static
+    {
+        $this->position = $position;
+
+        return $this;
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageFile(?File $imageFile): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            $this->imageUpdatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getImageUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->imageUpdatedAt;
+    }
+
+    public function setImageUpdatedAt(?\DateTimeImmutable $imageUpdatedAt): void
+    {
+        $this->imageUpdatedAt = $imageUpdatedAt;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): void
+    {
+        $this->image = $image;
+    }
+
+    public function getLanguage(): ?int
+    {
+        return $this->language;
+    }
+
+    public function setLanguage(?int $language): static
+    {
+        $this->language = $language;
+
+        return $this;
+    }
+}
---
+++ src/Form/ServiceType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Enum\LanguageEnum;
+use App\Entity\Service;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+
+class ServiceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('shortDescripton')
+            ->add('fullDescription')
+            ->add('status',  ChoiceType::class, [
+                'choices'  => [
+                    'Inativo' => 0,
+                    'Ativo' => 1,
+                ]
+            ])
+            ->add('highlighted',  ChoiceType::class, [
+                'choices'  => [
+                    'Não' => 0,
+                    'Sim' => 1,
+                ]
+            ])
+            ->add('position')
+            ->add('imageFile', VichImageType::class, [
+                'required'      => false,
+                'allow_delete'  => false,
+                'asset_helper'  => false,
+                'image_uri'     => false,
+                'download_uri'  => true,
+                'download_label'=> false,
+            ])
+            ->add('language', ChoiceType::class, [
+                'choices'  => LanguageEnum::getOptions(),
+            ]);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Service::class,
+        ]);
+    }
+}
