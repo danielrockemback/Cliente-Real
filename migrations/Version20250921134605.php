49ed827b52f315b2d22d58e140f56a09e640b85b
Daniel Borges <d.rockemback@gmail.com>
2025-09-21 11:33:48 -0300

    Criando Crud, Entity, Controller do Financiamento e arrumando o base e index do Dashboard

---
+++ migrations/Version20250921134605.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250921134605 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE financing (id INT IDENTITY NOT NULL, title NVARCHAR(255), short_descripton NVARCHAR(255), full_description VARCHAR(MAX), status BIT, highlighted BIT, position INT, image NVARCHAR(255), image_updated_at DATETIME2(6), language INT, PRIMARY KEY (id))');
+        $this->addSql('EXEC sp_addextendedproperty N\'MS_Description\', N\'(DC2Type:datetime_immutable)\', N\'SCHEMA\', \'dbo\', N\'TABLE\', \'financing\', N\'COLUMN\', \'image_updated_at\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA db_accessadmin');
+        $this->addSql('CREATE SCHEMA db_backupoperator');
+        $this->addSql('CREATE SCHEMA db_datareader');
+        $this->addSql('CREATE SCHEMA db_datawriter');
+        $this->addSql('CREATE SCHEMA db_ddladmin');
+        $this->addSql('CREATE SCHEMA db_denydatareader');
+        $this->addSql('CREATE SCHEMA db_denydatawriter');
+        $this->addSql('CREATE SCHEMA db_owner');
+        $this->addSql('CREATE SCHEMA db_securityadmin');
+        $this->addSql('CREATE SCHEMA dbo');
+        $this->addSql('DROP TABLE financing');
+    }
+}
--- src/Controller/admin/DashboardController.php
+++ src/Controller/admin/DashboardController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller\admin;
 
+use App\Repository\BannerRepository;
+use App\Repository\NewsRepository;
+use App\Repository\ProductRepository;
+use App\Repository\TestimonyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +13,25 @@ use Symfony\Component\Routing\Attribute\Route;
 final class DashboardController extends AbstractController
 {
     #[Route('/admin/dashboard', name: 'app_admin_dashboard')]
-    public function index(): Response
+    public function index(
+        BannerRepository $bannerRepository,
+        NewsRepository $newsRepository,
+        ProductRepository $productRepository,
+        TestimonyRepository $testimonyRepository
+    ): Response
     {
+        $totalBanners = sizeof($bannerRepository->findAll());
+        $totalNews = sizeof($newsRepository->findAll());
+        $totalProducts = sizeof($productRepository->findAll());
+        $totalTestimony = sizeof($testimonyRepository->findAll());
+
         return $this->render('admin/dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
+            'totalBanners' => $totalBanners,
+            'totalNews' => $totalNews,
+            'totalProducts' => $totalProducts,
+            'totalTestimony' => $totalTestimony,
+            'news' => $newsRepository->findBy([], ['date' => 'DESC']),
         ]);
     }
 }
